0c0345c166c2483bed7e08b132558e5873cc7497
Tiago Santos <a22006933@alunos.ulht.pt>
2022-04-13 22:53:25 +0100

    Created dashboard and new fire form

--- app/src/main/java/com/example/fogospt/ui/new_fire/NewfireViewModel.kt
+++ app/src/main/java/com/example/fogospt/ui/new_fire/NewfireViewModel.kt
@@ -1,13 +1,7 @@
 package com.example.fogospt.ui.new_fire
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class NewfireViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
-    }
-    val text: LiveData<String> = _text
 }
