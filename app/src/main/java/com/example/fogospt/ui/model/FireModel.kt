d3dc721feb94635c21153345736145610738d691
Tiago Santos <a22006933@alunos.ulht.pt>
2022-04-13 23:54:26 +0100

    Changed the constructos of FireModel

--- app/src/main/java/com/example/fogospt/ui/model/FireModel.kt
+++ app/src/main/java/com/example/fogospt/ui/model/FireModel.kt
@@ -2,29 +2,13 @@ package com.example.fogospt.ui.model
 
 import java.util.*
 
-class FireModel {
-
-    var name: String
-    var cartaoCidadao: Long
-    var destrito: String
-    var data: Date
-    var hora: String
+class FireModel(
+    var name: String,
+    var cartaoCidadao: Long,
+    var destrito: String,
+    var data: Date,
+    var hora: String,
     var fotografia: String
-
-    constructor(
-        name: String,
-        cartaoCidadao: Long,
-        destrito: String,
-        data: Date,
-        hora: String,
-        fotografia: String
-    ) {
-        this.name = name
-        this.cartaoCidadao = cartaoCidadao
-        this.destrito = destrito
-        this.data = data
-        this.hora = hora
-        this.fotografia = fotografia
-    }
+) {
 
 }
