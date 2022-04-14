cf753233a9905b9b8368db49693931754ea1b7c4
Tiago Santos <a22006933@alunos.ulht.pt>
2022-04-14 02:04:19 +0100

    Added the function to insert in firesList from form

--- app/src/main/java/com/example/fogospt/ui/model/FiresModel.kt
+++ app/src/main/java/com/example/fogospt/ui/model/FiresModel.kt
@@ -1,9 +1,14 @@
 package com.example.fogospt.ui.model
 
-class FiresModel {
+import java.util.*
 
-    companion object {
-        val listOfFires = listOf<FireModel>()
+object FiresModel{
+
+    val listOfFires: MutableList<FireModel> = mutableListOf()
+
+    init {
+        listOfFires.add(FireModel("Tiago",12345678, "Lisboa", Date(2022, 3,5), "17:00", "aaaa"))
+        listOfFires.add(FireModel("Ana",87654321, "Porto", Date(2022, 4,6), "09:32", "bbbb"))
     }
 
 }
--- app/src/main/java/com/example/fogospt/ui/new_fire/NewfireFragment.kt
+++ app/src/main/java/com/example/fogospt/ui/new_fire/NewfireFragment.kt
@@ -1,18 +1,21 @@
 package com.example.fogospt.ui.new_fire
 
-import android.app.DatePickerDialog
 import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
+import android.widget.DatePicker
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.fogospt.R
 import com.example.fogospt.databinding.FragmentNewFireBinding
-import org.w3c.dom.Text
+import com.example.fogospt.ui.model.FireModel
+import com.example.fogospt.ui.model.FiresModel.listOfFires
+import java.text.SimpleDateFormat
 import java.util.*
 
 class NewfireFragment : Fragment() {
@@ -26,6 +29,22 @@ class NewfireFragment : Fragment() {
     ): View {
         _binding = FragmentNewFireBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val btnSubmit: Button = root.findViewById(R.id.button_submit_form)
+        val name: Editable? = root.findViewById<EditText>(R.id.new_fire_person_name).getText()
+        val cc: Editable? = root.findViewById<EditText>(R.id.new_fire_person_cc).getText()
+        val district: Editable? = root.findViewById<EditText>(R.id.new_fire_district).getText()
+        val fireDate = root.findViewById<DatePicker>(R.id.dpDate)
+        val foto: Editable? = root.findViewById<EditText>(R.id.new_fire_photo).getText()
+
+        val date = Date(fireDate.getYear() - 1900, fireDate.getMonth(), fireDate.getDayOfMonth())
+
+        btnSubmit.setOnClickListener {
+            listOfFires.add(FireModel(name.toString(), cc.toString().toLong(), district.toString(), date, "17:00", foto.toString()))
+            for (fire in listOfFires) {
+                Log.d(TAG, "${fire.name} ${fire.cartaoCidadao} ${fire.destrito} ${fire.data} ${fire.hora} ${fire.fotografia}")
+            }
+        }
 
         return root
     }
